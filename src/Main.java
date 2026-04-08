a2567cfc947f5fb5df0c1bad7091fc9c0a7548b7
Labriis <blarisa795@gmail.com>
2026-04-08 15:24:03 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // 1. Criar Cliente
+        // Criar Cliente
         Cliente c1 = new Cliente("Ana Beatriz", 5);
 
-        // 2. Criar 3 Pedidos
+        // Criar 3 Pedidos
         Pedido ped1 = new Pedido("Pizza Grande", 45.00, 1);
         Pedido ped2 = new Pedido("Refrigerante", 8.00, 2);
         Pedido ped3 = new Pedido("Sobremesa", 18.00, 1);
 
-        // 3. Criar a Conta
+        // Criar a Conta
         ContaRestaurante conta = new ContaRestaurante(c1, ped1, ped2, ped3);
 
-        // 4. Exibir resultado
+        //Exibir resultado
         conta.exibirRelatorio();
     }
 }
